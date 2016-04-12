6279c475a04256bd12f1cd631a7fb9100ac86b6d
Anders Nickelsen <anders.nickelsen@gmail.com>
2016-04-12 15:28:40 +0200

    Base submodules.

--- Vagrantfile
+++ Vagrantfile
@@ -1,13 +1,13 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.network :private_network, type: "dhcp"
-  config.vm.hostname = "ggtracker"
+  config.vm.hostname = "ggtrackerstack"
 
   config.vm.provision "shell", path: "setup-vagrantbox.sh"
 
   config.vm.provider :virtualbox do |vb|
     vb.memory = 1536
     vb.cpus = 2
-    vb.name = "ggtracker"
+    vb.name = "ggtrackerstack"
   end
 end
--- setup-vagrantbox.sh
+++ setup-vagrantbox.sh
@@ -6,7 +6,7 @@ add-apt-repository ppa:chris-lea/redis-server -y
 
 apt-get update
 
-apt-get install -y ruby redis-server nodejs npm mysql-server git-core ruby-dev libcurl4-openssl-dev libmysqlclient-dev build-essential libxml2-dev libxslt-dev memcached imagemagick libsasl2-dev python-pip python-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
+apt-get install -y ruby redis-server nodejs npm mysql-server git-core ruby-dev libcurl4-openssl-dev libmysqlclient-dev build-essential libxml2-dev libxslt-dev memcached imagemagick libsasl2-dev python-pip python-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk 
 
 gem install bundler
 npm install -g juggernaut
