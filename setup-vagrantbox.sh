4196ad181a26ba9fd260c1915ac8e47253c09b54
Anders Nickelsen <anders.nickelsen@gmail.com>
2016-04-12 13:54:59 +0200

    Initial commit.

---
+++ setup-vagrantbox.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+add-apt-repository ppa:chris-lea/redis-server -y
+
+apt-get update
+
+apt-get install -y ruby redis-server nodejs npm mysql-server git-core ruby-dev libcurl4-openssl-dev libmysqlclient-dev build-essential libxml2-dev libxslt-dev memcached imagemagick libsasl2-dev python-pip python-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
+
+gem install bundler
+npm install -g juggernaut
+
+# Juggetnaut looks for `node` instead of `nodejs`
+sudo ln -s /usr/bin/nodejs /usr/bin/node
